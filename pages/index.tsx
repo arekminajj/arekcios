679c029c3ab3b5b4f45c5c95a230251bf62d87d5
arekminajj <arek123113@gmail.com>
2022-09-18 17:01:31 +0200

    king vamp

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <img className={styles.img} src="/img/saturn.png" />
+      <img className={styles.img} src="/img/carti_vamp.jpg" />
     </div>
   )
 }
